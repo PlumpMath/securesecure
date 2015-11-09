d9b2704c9ab4d3b838f7ff6f46b8be00ac9d59ea
Juan Matthys Uys <opyate@gmail.com>
2015-11-09 10:51:47 +0000

    width and height options

--- app.py
+++ app.py
@@ -11,11 +11,15 @@ Pushrod(app)
 @pushrod_view(jinja_template='index.html')
 def hello():
     url = request.args.get('url')
+    width = request.args.get('w') or 800
+    height = request.args.get('h') or 500
     if not url:
         return {}
     else:
         return {
-            'url': url
+            'url': url,
+            'width': width,
+            'height': height
         }
 
 if __name__ == '__main__':
